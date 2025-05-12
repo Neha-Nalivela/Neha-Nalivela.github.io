132b7d805b621db6ee6e598f6f192c6cf021d4fc
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-12 20:52:28 +0530

    update

---
+++ A6/script.js
@@ -0,0 +1,6 @@
+function msg()
+{
+    var email=document.getElementById('email').value;
+    var pass=document.getElementById('pass').value;
+    document.getElementById('op').innerHTML="E-Mail is "+email+"<br> Password is :"+pass;
+}
