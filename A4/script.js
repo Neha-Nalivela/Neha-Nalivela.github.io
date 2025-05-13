61d54e2895f0a73b82caa60275c56ba0b27ebcfe
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-13 21:00:44 +0530

    update

--- A4/script.js
+++ A4/script.js
@@ -1,3 +1,15 @@
-for(let i=3;i<=12;i+10){
-    let str=str+`<option>${i}</option>`;
+let str1="";
+let str2="";
+let str3="";
+for(let i=1;i<=31;i+10){
+     str1=str1+`<option>${i}</option>`;
 }
+date.innerHTML = str;
+for(let i=1;i<=12;i+10){
+     str2=str2+`<option>${i}</option>`;
+}
+mon.innerHTML = str2;
+for(let i=1;i<=31;i+10){
+     str3=str3+`<option>${i}</option>`;
+}
+year.innerHTML = str3;
