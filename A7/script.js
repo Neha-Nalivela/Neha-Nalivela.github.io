9016653dbf5f1b0421664f63d70b36b8f2cead08
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-13 19:44:13 +0530

    update

--- A7/script.js
+++ A7/script.js
@@ -2,9 +2,9 @@ function msg(){
     var num1=document.getElementById('num1').value;
     var num2 = document.getElementById('num2').value;
     var sum=parseFloat(num1)+parseFloat(num2);
-    var sub=parseFloat(num1)+parseFloat(num2);
-    var mul=parseFloat(num1)+parseFloat(num2);
-    var dov=parseFloat(num1)+parseFloat(num2);
+    var sub=parseFloat(num1)-parseFloat(num2);
+    var mul=parseFloat(num1)*parseFloat(num2);
+    var div=parseFloat(num1)/parseFloat(num2);
     document.getElementById('res').innerHTML=
     "Sum is:"+sum+"<br>"+
     "Sub is:"+sub+"<br>"+
