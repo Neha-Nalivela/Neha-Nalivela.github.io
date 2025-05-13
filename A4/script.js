3b760b81e2f34c2a16d2a5e0af6465b0c6f180e1
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-13 20:50:19 +0530

    update

---
+++ A4/script.js
@@ -0,0 +1,3 @@
+for(let i=3;i<=12;i+10){
+    let str=str+`<option>${i}</option>`;
+}
--- A7/script.js
+++ A7/script.js
@@ -1,13 +1,28 @@
-function msg(){
-    var num1=document.getElementById('num1').value;
-    var num2 = document.getElementById('num2').value;
-    var sum=parseFloat(num1)+parseFloat(num2);
-    var sub=parseFloat(num1)-parseFloat(num2);
-    var mul=parseFloat(num1)*parseFloat(num2);
-    var div=parseFloat(num1)/parseFloat(num2);
-    document.getElementById('res').innerHTML=
-    "Sum is:"+sum+"<br>"+
-    "Sub is:"+sub+"<br>"+
-    "Mul is:"+mul+"<br>"+
-    "Div is:"+div;
+for(let i=10;i<=100;i++){
+    let str=str+`<option>${i}</option>`;
 }
+num1.innerHTML=str
+const msg = () => {
+    var num1=parserFloat(document.getElementById('num1')).value;
+    var num2 = parserFloat(document.getElementById('num2')).value;
+    let r1=add(num1,num2);
+    let r2=sub(num1,num2);
+    let r3=mul(num1,num2);
+    let r4=div(num1,num2);
+    dvAdd.innerHTML="Addition"+r1;
+    dvSub.innerHTML="Substraction"+r2;
+    dvMul.innerHTML="Multiplication:"+r3;
+    dvDiv.innerHTML="Division:"+r4;
+};
+function add(num1,num2){
+    return num1+num2;
+};
+function sub(num1,num2){
+    return num1-num2
+};
+function mul(num1,num2){
+    return num1*num2;
+};
+function div(num1,num2){
+    return num1/num2;
+};
