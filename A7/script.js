bec3f0e3b395f9416428009da105a5e500bd3078
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-13 19:39:16 +0530

    update

---
+++ A7/script.js
@@ -0,0 +1,13 @@
+function msg(){
+    var num1=document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var sum=parseFloat(num1)+parseFloat(num2);
+    var sub=parseFloat(num1)+parseFloat(num2);
+    var mul=parseFloat(num1)+parseFloat(num2);
+    var dov=parseFloat(num1)+parseFloat(num2);
+    document.getElementById('res').innerHTML=
+    "Sum is:"+sum+"<br>"+
+    "Sub is:"+sub+"<br>"+
+    "Mul is:"+mul+"<br>"+
+    "Div is:"+div;
+}
