483071ef9e73aaf54e47b0362f888c10689e42b9
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-16 19:32:32 +0530

    update

---
+++ A12/index.js
@@ -0,0 +1,7 @@
+const products=[
+    { id: 1, name: "product1", price: 34},
+    { id : 2, name: "product2", price: 56},
+    { id : 3, name: "product2", price: 43}
+
+];
+products.foreach((product) => console.log(product.name))
