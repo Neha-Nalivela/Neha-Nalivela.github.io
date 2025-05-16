d42cfcb312d93164b8c43538aaed7d8302202dea
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-16 19:43:59 +0530

    update

---
+++ A13/index.js
@@ -0,0 +1,9 @@
+const products=[
+    { id: 1, name: "product1", price: 34},
+    { id : 2, name: "product2", price: 56},
+    { id : 3, name: "product2", price: 43}
+
+];
+
+const ins = products.map(value=>value+5)
+console.log(ins)
