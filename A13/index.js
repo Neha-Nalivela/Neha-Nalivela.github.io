3cb5aeeadf1f9b635e2c8eb110f64351640a7326
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-16 22:46:56 +0530

    update

--- A13/index.js
+++ A13/index.js
@@ -1,3 +1,9 @@
-fetch("https://jsonplaceholder.typicode.com/posts")
-.then((res) => res.json()
-.then((data) => console.log(data)));
+ fetch("https://jsonplaceholder.typicode.com/posts")//fetch(url)
+ .then((res) => res.json()//to get in json format
+ .then((data) => console.log(data)));//display
+
+
+
+// // Get info grom remote server
+// // Convert data into json format
+// // Display the converting data
