9c68e62698ecbb8cd2f4c71fa2c5e7597fd5bec8
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-17 20:38:30 +0530

    update

--- A15/index.js
+++ A15/index.js
@@ -1,15 +1,11 @@
-
 function greet(){
-    setTimeout(()=>{
-        console.log("Hello World")
-    },2000);
-  console.log("Hello World")
+    console.log("Hello World")
 }
  
 async function dispData() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json()
   console.log(data);
+  greet();
 }
 dispData();
-greet()
