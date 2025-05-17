9ef5c409c3982b94e7e250481323a880d6e0b621
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-17 18:51:17 +0530

    update

--- A13/index.js
+++ A13/index.js
@@ -1,11 +1,11 @@
 const products=[
-    { id: 1, name: "product1", price: 34},
-    { id : 2, name: "product2", price: 56},
-    { id : 3, name: "product2", price: 43}
+    { id: 1, name: "product1", price: 34, status:"pending"},
+    { id : 2, name: "product2", price: 56, status:"pending"},
+    { id : 3, name: "product2", price: 43, status:"pending"},
 
 ];
 //products.foreach((product) => console.log(product.name))
 
 
-const ins = products.map(price=>[price.price+5])
+const ins = products.map((product)=>({...product,price:product.price+5, status: "completed"}));
 console.log(ins)
