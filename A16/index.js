1c459e642a4aa0c5e0717c0b795dfc616ee409ee
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-17 20:54:26 +0530

    update

--- A16/index.js
+++ A16/index.js
@@ -1,9 +1,15 @@
-function add(x,y){
-    return Promise.resolve(x+y)
+function add(x, y) {
+    if (x < 5) {
+        return Promise.reject("X is less than 5");
+    } else {
+        return Promise.resolve(x + y);
+    }
 }
-function sqr(a){
-    return a*a
+
+function sqr(a) {
+    return a * a;
 }
+
 async function main(){
     let sum = await add(4,5)
     let res= sqr(sum);
