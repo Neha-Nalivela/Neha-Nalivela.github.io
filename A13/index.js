8760a6c17d98937279dc426c476b1a095f9f2ee6
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-17 19:31:14 +0530

    update

--- A13/index.js
+++ A13/index.js
@@ -1,14 +1,22 @@
 const products=[
-    { id: 1, name: "product1", qty:3, price: 34, status:"pending"},
-    { id : 2, name: "product2", qty:5,  price: 56, status:"pending"},
-    { id : 3, name: "product3", qty:2, price: 43, status:"pending"},
+    { id: 1, name: "product1", price: 34},
+    { id : 2, name: "product2", price: 56},
+    { id : 3, name: "product3",  price: 43},
 
 ];
-const ins = products.filter(product=>product.qty>2).map((product)=>({
-    ...product,
-    price:product.price+5, 
-    status: "completed", 
-    total:product.price*product.qty})
-);
-console.log(ins)
+
+const cart={1:7,3:5}//id:qty
+products.map(value=>{
+    const qty = cart[value.id]||0;
+    //const total:qty*value.price
+    console.log(value.id, value.name, value.price, value.price*qty)
+})
+
+//const ins = products.filter(product=>product.qty>2).map((product)=>({
+//    ...product,
+//    price:product.price+5, 
+//    status: "completed", 
+//    total:product.price*product.qty})
+//);
+//console.log(ins)
 //products.foreach((product) => console.log(product.name))
