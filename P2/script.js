bfd73cbc827ad2a5c5fcbe7fa729f108e2ccbb62
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-18 20:52:02 +0530

    update

--- P2/script.js
+++ P2/script.js
@@ -4,10 +4,63 @@ const products = [
   { id: 3, name: "p3", price: 75 },
 ];
 
+const cart = {};
+
+const addToCart = (id) => {
+  /*
+  if (cart[id]) {
+    cart[id]++;
+  } else {
+    cart[id] = 1;
+  }
+  */
+ //cart[id]=1;
+ cart[id] = (cart[id] || 0)+1;
+ console.log(cart);
+};
+
+const dispCart = () => {
+ const root = document.getElementById("root");
+ /* root.innerHTML = "<h2>Cart</h2>";
+
+  for (let id in cart) {
+    const product = products.find(p => p.id == id);
+    root.innerHTML += `
+      <div class="product">
+        <p>${product.name}</p>
+        <p>₹${product.price}</p>
+        <p>Quantity: ${cart[id]}</p>
+      </div>
+    `;
+  }*/
+ let str = "<h2>Cart</h2>";
+  products.map((value) => {
+    cart[value.id] &&
+      (str += `<div class="product">
+      ${value.name}-${value.price}-<button onclick="dec(${value.id})">-</button>${cart[value.id]}<button onclick="inc(${value.id})">+</button>-${value.price*cart[value.id]}
+     </div>
+    `);
+  });
+  root.innerHTML = str;
+};
+
+const inc = (qty) => {
+    cart[qty]++;
+    dispCart();
+}
+const dec = (qty) => {
+    if (cart[qty] > 1) {
+    cart[qty]--;
+  } else {
+    delete cart[qty];
+  }
+  dispCart();
+}
 const showProducts = () => {
   const root = document.getElementById("root");
+  root.innerHTML = "<h2>Product List</h2>";
 
-  products.map((value) => {
+  products.forEach((value) => {
     root.innerHTML += `
       <div class="product">
         <p>${value.name}</p>
@@ -16,8 +69,4 @@ const showProducts = () => {
       </div>
     `;
   });
-};
-
-const addToCart = (id) => {
-  alert("Product " + id + " added to cart!");
 };
