2893ca9d57dd36f86bdfc817c5b7c30f6d9e6e88
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-18 18:54:58 +0530

    update

---
+++ P2/index.js
@@ -0,0 +1,8 @@
+const products = [
+    { id: 1, name: "product1", price: 34 },
+    { id: 2, name: "product2", price: 56 },
+    { id: 3, name: "product3", price: 43 },
+];
+const showProducts = () => {
+    root.innerHTML("Product List")
+};
